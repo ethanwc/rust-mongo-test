e997568c6c8f54c64aa0cf6c15f981124c8f363c
ethanwc <ethanwc@u.washington.edu>
2020-07-08 00:02:00 -0700

    connected to mongo

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use mongodb::{Client, options::ClientOptions};
 use std::io;
+use futures::executor::block_on;
+use mongodb::bson::doc;
 
 fn main() -> Result<(), io::Error> {
     println!("Hello, world!");
-    test();
+    let future = test();
+    block_on(future);
     Ok(())
 }
 
 async fn test() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
+    let mut client_options = ClientOptions::parse("mongodb+srv://ourrecipes-temp:Vpzk0A4RUxHRsWYB@cluster0.1bw4q.mongodb.net/<dbname>?retryWrites=true&w=majority").await?;
     let client = Client::with_options(client_options)?;
 
     for db_name in client.list_database_names(None, None).await? {
